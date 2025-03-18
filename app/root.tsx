20cf30d715b9528a8cd4711100a9ec435ad7ce3e
Morshed <morshed.monsterstudio@gmail.com>
2025-03-18 11:01:49 +0600

    remove dark theme for now

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
-import { ThemeProvider } from "./components/provider/ThemeProvider";
+
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,9 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
